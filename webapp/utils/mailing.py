8cbd9b7cc8348005fe1d3599dabaa7458b909735
Dmytruk <dima.dmytruk23@gmail.com>
2020-03-18 11:54:20 +0200

    black fix

--- webapp/utils/mailing.py
+++ webapp/utils/mailing.py
@@ -4,9 +4,11 @@ import sendgrid
 from sendgrid.helpers.mail import Content, Email, Mail, To
 
 
-def sengrid_send_mail(email: str, subject: str, content: str, content_type: str):
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENGRID_API_KEY'))
-    from_email = Email(os.environ.get('SENGRID_EMAIL'))
+def sengrid_send_mail(
+    email: str, subject: str, content: str, content_type: str
+):
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENGRID_API_KEY"))
+    from_email = Email(os.environ.get("SENGRID_EMAIL"))
     to_email = To(email)
     content = Content(content_type, content)
     mail = Mail(from_email, to_email, subject, content)
