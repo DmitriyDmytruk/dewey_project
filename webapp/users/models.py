1f588f6ff51574b41ba074594ef370f2165d5058
Dmytruk <dima.dmytruk23@gmail.com>
2020-03-19 15:02:19 +0200

    token generation for API User role without expire date

--- webapp/users/models.py
+++ webapp/users/models.py
@@ -6,6 +6,56 @@ from flask import current_app
 from .. import bcrypt, db
 
 
+class PermissionModel(db.Model):
+    """
+    User Permission Model
+    """
+
+    __tablename__ = "permissions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), unique=True, nullable=False)
+
+    def __init__(self, title):
+        self.title = title
+
+    def save(self):
+        """
+        Save permission
+        :return:
+        """
+        db.session.add(self)
+        db.session.commit()
+
+
+role_permissions = db.Table(
+    "role_permissions",
+    db.Column(
+        "permission_id",
+        db.Integer,
+        db.ForeignKey("permissions.id"),
+        primary_key=True,
+    ),
+    db.Column(
+        "role_id", db.Integer, db.ForeignKey("roles.id"), primary_key=True
+    ),
+)
+
+
+class RoleModel(db.Model):
+    """
+    User Role Model
+    """
+
+    __tablename__ = "roles"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), unique=True, nullable=False)
+    permissions = db.relationship(
+        PermissionModel, secondary=role_permissions
+    )
+
+
 class UserModel(db.Model):
     """
     User Model
@@ -45,19 +95,17 @@ class UserModel(db.Model):
         :return: string
         """
         try:
-            payload = {
-                # fmt: off
-                "exp": datetime.datetime.utcnow()
-                       + datetime.timedelta(days=0, seconds=5),
-                # fmt: on
-                "iat": datetime.datetime.utcnow(),
-                "sub": self.email,
-            }
-            return jwt.encode(
-                payload,
-                current_app.config.get("SECRET_KEY"),
-                algorithm="HS256",
-            )
+            api_user_role = RoleModel.query.filter_by(title='API User').one()
+            if self.role_id and api_user_role.id == self.role_id:
+                payload = {
+                    "iat": datetime.datetime.utcnow(),
+                    "sub": self.email,
+                }
+                return jwt.encode(
+                    payload,
+                    current_app.config.get("SECRET_KEY"),
+                    algorithm="HS256",
+                )
         except Exception as err:  # pylint: disable=broad-except
             return err
 
@@ -74,53 +122,3 @@ class UserModel(db.Model):
         :return: boolean | None
         """
         return bcrypt.check_password_hash(self.password, password)
-
-
-role_permissions = db.Table(
-    "role_permissions",
-    db.Column(
-        "permission_id",
-        db.Integer,
-        db.ForeignKey("permissions.id"),
-        primary_key=True,
-    ),
-    db.Column(
-        "role_id", db.Integer, db.ForeignKey("roles.id"), primary_key=True
-    ),
-)
-
-
-class RoleModel(db.Model):
-    """
-    User Role Model
-    """
-
-    __tablename__ = "roles"
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), unique=True, nullable=False)
-    permissions = db.relationship(
-        "PermissionModel", secondary=role_permissions
-    )
-
-
-class PermissionModel(db.Model):
-    """
-    User Permission Model
-    """
-
-    __tablename__ = "permissions"
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(256), unique=True, nullable=False)
-
-    def __init__(self, title):
-        self.title = title
-
-    def save(self):
-        """
-        Save permission
-        :return:
-        """
-        db.session.add(self)
-        db.session.commit()
