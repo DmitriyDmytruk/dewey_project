da8a4a54824869987040e0c38a16eed70dad9855
Irina Mityayeva <irina.mityayeva.ddi@gmail.com>
2020-04-17 12:02:15 +0300

    Update article models and requirements

--- webapp/articles/models.py
+++ webapp/articles/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy_utils import ScalarListType
+
 from webapp import db
 
 from .mixins import SearchableMixin
@@ -76,6 +78,7 @@ class ArticleModel(db.Model, SearchableMixin):
         "local_regulation",
         "abstract",
         "categories",
+        "reference_images",
         "effective_date",
         "updated_date",
         "state",
@@ -93,6 +96,7 @@ class ArticleModel(db.Model, SearchableMixin):
     local_regulation = db.Column(db.Text)
     abstract = db.Column(db.Text)
     categories = db.relationship(CategoryModel, secondary=article_categories)
+    reference_images = db.Column(ScalarListType())
     effective_date = db.Column(db.Date)
     updated_date = db.Column(db.Date)
     tags = db.relationship(TagModel, secondary=article_tags)
