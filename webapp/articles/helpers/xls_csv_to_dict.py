189f7ebc4e9f97142da11429c9af5bd71c2817f8
DmitriyDmytruk <96Jun_kasai3>
2020-05-27 11:31:02 +0300

    xls/csv diff commented

--- webapp/articles/helpers/xls_csv_to_dict.py
+++ webapp/articles/helpers/xls_csv_to_dict.py
@@ -5,8 +5,6 @@ from typing import IO, Any, Dict, List, Tuple, Union
 from xlrd import Book, open_workbook
 from xlrd.sheet import Sheet
 
-from ..models import ArticleModel
-
 
 def columns_mapping(origin_dict: dict) -> Dict:
     """
@@ -117,86 +115,86 @@ class CSVReader:
         return dict_list
 
 
-class ArticleDiff:
-    """
-    Checks articles changes
-    """
-
-    def __init__(self) -> None:
-        self.imported_data = {}
-        self.exist_articles_list = []
-        self.new_articles_list = []
-        self.diff = {}
-        self.keys = []
-        self.rows_list = []
-        self.sheet = Sheet
-
-    def get_differences(self) -> None:
-        """
-        Checks articles changes
-        """
-        unique_id = self.imported_data["unique_id"]
-        article = ArticleModel.query.filter_by(
-            unique_id=unique_id
-        ).one_or_none()
-        if article:
-            self.exist_articles_list.append(article)
-            model_data = {}
-            for column in article.__table__.columns:
-                model_data[column.name] = str(getattr(article, column.name))
-            imported_categories = set(
-                cat["name"] for cat in self.imported_data["categories"]
-            )
-            categories = set(r.name for r in article.categories)
-            if imported_categories != categories:
-                new_categories = imported_categories - categories
-                self.diff[unique_id] = {"categories": new_categories}
-            imported_tags = set(
-                tag["name"] for tag in self.imported_data["tags"]
-            )
-            tags = set(r.name for r in article.tags)
-            if imported_tags != tags:
-                new_tags = imported_tags - tags
-                self.diff[unique_id].update(tags=new_tags)
-            del self.imported_data["categories"]
-            del self.imported_data["tags"]
-            self.diff[unique_id].update(
-                {
-                    k: self.imported_data[k]
-                    for k, v in (
-                        set(self.imported_data.items())
-                        - set(model_data.items())
-                    )
-                }
-            )
-        else:
-            self.new_articles_list.append(self.imported_data)
-
-    def xls_diff(self, file: IO) -> Dict[str, str]:
-        """
-        Prepare .xls data
-        """
-        self.sheet, keys = XLSReader().open(file)
-        for row_index in range(1, self.sheet.nrows):
-            d = {
-                keys[col_index]
-                .lower()
-                .replace(" ", "_"): self.sheet.cell(row_index, col_index)
-                .value
-                for col_index in range(self.sheet.ncols)
-            }
-            self.imported_data = columns_mapping(d)
-            self.get_differences()
-        return self.diff
-
-    def csv_diff(self, file: IO) -> Dict[str, str]:
-        """
-        Prepare .csv data
-        """
-        self.rows_list, self.keys = CSVReader().open(file)
-        for rows in self.rows_list[1:]:
-            for index, item in enumerate(rows):
-                self.imported_data[self.keys[index]] = item
-            self.imported_data = columns_mapping(self.imported_data)
-            self.get_differences()
-        return self.diff
+# class ArticleDiff:
+#     """
+#     Checks articles changes
+#     """
+#
+#     def __init__(self) -> None:
+#         self.imported_data = {}
+#         self.exist_articles_list = []
+#         self.new_articles_list = []
+#         self.diff = {}
+#         self.keys = []
+#         self.rows_list = []
+#         self.sheet = Sheet
+#
+#     def get_differences(self) -> None:
+#         """
+#         Checks articles changes
+#         """
+#         unique_id = self.imported_data["unique_id"]
+#         article = ArticleModel.query.filter_by(
+#             unique_id=unique_id
+#         ).one_or_none()
+#         if article:
+#             self.exist_articles_list.append(article)
+#             model_data = {}
+#             for column in article.__table__.columns:
+#                 model_data[column.name] = str(getattr(article, column.name))
+#             imported_categories = set(
+#                 cat["name"] for cat in self.imported_data["categories"]
+#             )
+#             categories = set(r.name for r in article.categories)
+#             if imported_categories != categories:
+#                 new_categories = imported_categories - categories
+#                 self.diff[unique_id] = {"categories": new_categories}
+#             imported_tags = set(
+#                 tag["name"] for tag in self.imported_data["tags"]
+#             )
+#             tags = set(r.name for r in article.tags)
+#             if imported_tags != tags:
+#                 new_tags = imported_tags - tags
+#                 self.diff[unique_id].update(tags=new_tags)
+#             del self.imported_data["categories"]
+#             del self.imported_data["tags"]
+#             self.diff[unique_id].update(
+#                 {
+#                     k: self.imported_data[k]
+#                     for k, v in (
+#                         set(self.imported_data.items())
+#                         - set(model_data.items())
+#                     )
+#                 }
+#             )
+#         else:
+#             self.new_articles_list.append(self.imported_data)
+#
+#     def xls_diff(self, file: IO) -> Dict[str, str]:
+#         """
+#         Prepare .xls data
+#         """
+#         self.sheet, keys = XLSReader().open(file)
+#         for row_index in range(1, self.sheet.nrows):
+#             d = {
+#                 keys[col_index]
+#                 .lower()
+#                 .replace(" ", "_"): self.sheet.cell(row_index, col_index)
+#                 .value
+#                 for col_index in range(self.sheet.ncols)
+#             }
+#             self.imported_data = columns_mapping(d)
+#             self.get_differences()
+#         return self.diff
+#
+#     def csv_diff(self, file: IO) -> Dict[str, str]:
+#         """
+#         Prepare .csv data
+#         """
+#         self.rows_list, self.keys = CSVReader().open(file)
+#         for rows in self.rows_list[1:]:
+#             for index, item in enumerate(rows):
+#                 self.imported_data[self.keys[index]] = item
+#             self.imported_data = columns_mapping(self.imported_data)
+#             self.get_differences()
+#         return self.diff
